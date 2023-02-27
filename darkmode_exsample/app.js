70a2fd5fd4e457899f3777ae1fbc8e057a3f4ae0
SimenH98 <simenhelgeby@hotmail.com>
2023-02-27 15:04:43 +0100

    Update app.js

--- darkmode_exsample/app.js
+++ darkmode_exsample/app.js
@@ -4,12 +4,12 @@ const moonPath =
 const sunPath = 
     "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
 
-const darkMode = document.querySelector('#darkMode');
+const darkMode = document.querySelector("#darkMode");
 let toggle = false;
 
 //We need to click on the sun
 
-darkMode.addEventListener('click', () => {
+darkMode.addEventListener("click", () => {
     //We need to use anime.js
     //Here we set up the timeline
     const timeline = anime.timeline({
@@ -19,10 +19,25 @@ darkMode.addEventListener('click', () => {
     //Add different animations to the timeline
     timeline.add({
         targets : ".sun",
-        d: [{value: moonPath}]
+        d: [{value: toggle ? sunPath : moonPath}]
     })
     .add({
         targets: "#darkMode",
         rotate: 320
-    }, '-= 350');
+    },
+     "-= 350"
+    )
+    .add({
+        targets: "section",
+        backgroundColor: toggle ? "rgb(255, 255, 255)" : "rgb(22,22,22)",
+        color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)"
+    }, 
+    "-= 700" 
+    );
+    //Everytime we click on the sun I want that toggle to switch
+    if(!toggle){
+        toggle = true;
+    }else{
+        toggle = false;
+    }
 });
